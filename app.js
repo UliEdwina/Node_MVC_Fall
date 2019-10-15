b03cd486e05b648c3874aad9639b5572fad7498f
UliEdwina <uli.orduno@codeimmersives.com>
2019-10-14 23:52:30 -0400

    pending a TypeError from .bin

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
+var logger = require('morgan');
+let mongoose     = require('mongoose');
+let session          = require('express-session');
+let MongoStore = require('connect-mongo')(session);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
+
+//confi env
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true } )
+        .then( () => {
+          console.log('MONGODB CONNECTED')
+        })
+        .catch( err => console.log(`ERROR: ${err}`))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +31,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Uses Mongostore
+
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+    store: new MongoStore({ url: process.env.MONGODB_URI, autoReconnect: true}),
+    cookie: {
+        secure: false, 
+        maxAge: 365 * 24 * 60 * 60 * 1000
+    }
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
